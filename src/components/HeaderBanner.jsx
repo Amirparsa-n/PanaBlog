a41f49ab07838f3ce94f47853d29cda3cded5d4d
Amirparsa Naghibi <amirparsanaghibi@gmail.com>
2023-07-14 16:12:04 +0330

    Header Banner in mobile created.
    Building a carousel using the Swiper library

--- src/components/HeaderBanner.jsx
+++ src/components/HeaderBanner.jsx
@@ -5,6 +5,14 @@ import { Link } from 'react-router-dom';
 // mui
 import { Box, Grid, Typography } from '@mui/material';
 
+// Swiper
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper/modules';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
 
 const HeaderBanner = ({posts}) => {
 
@@ -19,10 +27,9 @@ const HeaderBanner = ({posts}) => {
         }
     }
 
-    console.log(randomPosts);
-
     return (
-        <Grid container spacing={3} mt={16} height={'600px'} marginTop={{xs: 4, md: 12}}>
+        <>
+        <Grid container display={{xs: 'none', sm: 'flex'}} spacing={3} height={'600px'} marginTop={{xs: 4, md: 12}}>
             <Grid item xs={12} md={5}>
                 <Link to={randomPosts[0].slug}>
                     <Box component={'div'} height={'600px'} sx={{background: `linear-gradient(180deg, rgba(255,255,255,0) 0%, rgba(0,0,0,0.9) 100%) ,url(${randomPosts[0].coverPhoto.url}) no-repeat top`, backgroundSize: 'cover', borderRadius: '20px', position: 'relative'}}>
@@ -55,6 +62,56 @@ const HeaderBanner = ({posts}) => {
                 </Link>
             </Grid>
         </Grid>
+
+        <div className='BannerHeader'>
+
+            <Swiper
+              pagination={{
+                type: 'progressbar',
+              }}
+              autoplay={{
+                delay: 3000,
+                disableOnInteraction: false,
+              }}
+              navigation={true}
+              modules={[Pagination, Navigation, Autoplay]}
+              className="mySwiper"
+            >
+                <SwiperSlide >
+                    <Link to={randomPosts[0].slug}>
+                        <Box component={'div'} height={'600px'} sx={{background: `linear-gradient(180deg, rgba(255,255,255,0) 0%, rgba(0,0,0,0.9) 100%) ,url(${randomPosts[0].coverPhoto.url}) no-repeat top`, backgroundSize: 'cover', borderRadius: '20px', position: 'relative'}}>
+                            <div >
+                                <Typography px={2} component={'h3'} variant='h5' lineHeight={2} color={'white'} fontWeight={700} textAlign={'center'} position={'absolute'} bottom={'80px'}>
+                                    {randomPosts[0].title}
+                                </Typography>
+                            </div> 
+                        </Box>
+                    </Link>
+                </SwiperSlide>
+                
+                <SwiperSlide>
+                    <Link to={randomPosts[1].slug}>
+                        <Box component={'div'} height={'600px'} sx={{background: `linear-gradient(180deg, rgba(255,255,255,0) 0%, rgba(0,0,0,0.9) 100%) ,url(${randomPosts[1].coverPhoto.url}) no-repeat top`, backgroundSize: 'cover', borderRadius: '20px', position: 'relative'}}>
+                            <Typography px={2} component={'h3'} variant='h5' lineHeight={2} color={'white'} fontWeight={700} textAlign={'center'} position={'absolute'} bottom={'80px'}>
+                                {randomPosts[1].title}
+                            </Typography>
+                        </Box>
+                    </Link>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <Link to={randomPosts[2].slug}>
+                        <Box component={'div'} height={'600px'} sx={{background: `linear-gradient(180deg, rgba(255,255,255,0) 0%, rgba(0,0,0,0.9) 100%) ,url(${randomPosts[2].coverPhoto.url}) no-repeat top`, backgroundSize: 'cover', borderRadius: '20px', position: 'relative'}}>
+                            <Typography px={2} component={'h3'} variant='h5' lineHeight={2} color={'white'} fontWeight={700} textAlign={'center'} position={'absolute'} bottom={'80px'}>
+                                    {randomPosts[2].title}
+                            </Typography>
+                        </Box>
+                    </Link>
+                </SwiperSlide>
+
+            </Swiper>
+        </div>
+        </>
     );
 };
 
