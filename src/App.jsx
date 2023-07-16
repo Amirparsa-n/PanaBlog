644e329625dbfd3719c4031e14751c9754c12c61
Amirparsa Naghibi <amirparsanaghibi@gmail.com>
2023-07-16 16:55:27 +0330

    Added ScrollToTop

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,13 @@ import HomePage from './page/HomePage';
 import Footer from './components/layout/Footer';
 import Blogs from './page/Blogs';
 
+import ScrollToTop from './components/shared/ScrollToTop';
+
 function App() {
 
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path='/' element={<HomePage />} />
